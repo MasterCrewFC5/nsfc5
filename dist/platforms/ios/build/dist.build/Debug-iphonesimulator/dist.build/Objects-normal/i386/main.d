a8c6ef6859a8afe4d6dab35338ca59ffb030c43d
Joe Rbr <johribe@gmail.com>
2018-03-29 10:52:45 +0200

    initial commit

---
+++ dist/platforms/ios/build/dist.build/Debug-iphonesimulator/dist.build/Objects-normal/i386/main.d
@@ -0,0 +1,10 @@
+dependencies: /root/app/FC5-ns/dist/platforms/ios/dist/dist-Prefix.pch \
+  /root/app/FC5-ns/dist/platforms/ios/internal/main.m \
+  /root/app/FC5-ns/dist/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /root/app/FC5-ns/dist/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /root/app/FC5-ns/dist/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /root/app/FC5-ns/dist/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /root/app/FC5-ns/dist/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /root/app/FC5-ns/dist/platforms/ios/internal/TNSExceptionHandler.h \
+  /root/app/FC5-ns/dist/platforms/ios/internal/TNSDebugging.h \
+  /root/app/FC5-ns/dist/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
